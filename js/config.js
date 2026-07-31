bcefc774cbfac23ccd5d49e6d0741f8b9e4eb68c
vinhhungdesign2345-tech <vinhhungdesign2345@gmail.com>
2026-07-31 16:40:08 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,37 @@
 // js/config.js
 const CONFIG = {
-    // URL Google Apps Script Web App mới của bạn
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz87dcUkndM5w5BeFqUFYJt8JDEcPu98IH5mbzNdov_6eXTNUEhIiknFQ9P7H2c0ZQE/exec",
+    // Đường dẫn đọc file GeoJSON nội bộ trên GitHub
+    GEOJSON_URL: "./Ca-Mau.geojson",
     
+    // Nền bản đồ Google Vệ tinh (Google Satellite) mới nhất
+    MAP_STYLE: {
+        'version': 8,
+        'sources': {
+            'google-satellite': {
+                'type': 'raster',
+                'tiles': [
+                    'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+                ],
+                'tileSize': 256
+            }
+        },
+        'layers': [
+            {
+                'id': 'google-satellite-layer',
+                'type': 'raster',
+                'source': 'google-satellite',
+                'minzoom': 0,
+                'maxzoom': 22
+            }
+        ]
+    },
+
     // Tọa độ trung tâm Cà Mau & Zoom mặc định
     MAP_CENTER: [105.15, 9.18],
     MAP_ZOOM: 12,
     
-    // Màu sắc thửa đất
-    FILL_COLOR: '#28a745',
-    FILL_OPACITY: 0.5,
-    OUTLINE_COLOR: '#000000'
+    // Viền thửa đất màu vàng chanh nổi bật trên nền Vệ tinh
+    FILL_COLOR: '#00ffcc',
+    FILL_OPACITY: 0.3,
+    OUTLINE_COLOR: '#ffffff'
 };
