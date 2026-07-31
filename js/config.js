a70f53904dfba889b540c11e18f04df20aff0ae8
vinhhungdesign2345-tech <vinhhungdesign2345@gmail.com>
2026-07-31 17:06:25 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // js/config.js
 const CONFIG = {
+    // Danh sách Tỉnh/Thành phố hỗ trợ
     PROVINCES: [
         { id: "CaMau", name: "Cà Mau", file: "./geojson/Ca-Mau.geojson", center: [105.15, 9.18] }
     ],
-};
 
     // Nền bản đồ Google Vệ tinh
     MAP_STYLE: {
