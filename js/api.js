c5d67af0dea3a5ba10719b2f8d321c993c74b57e
vinhhungdesign2345-tech <vinhhungdesign2345@gmail.com>
2026-07-31 17:06:55 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,6 +2,9 @@
 async function fetchGeoDataByUrl(fileUrl) {
     try {
         const response = await fetch(fileUrl);
+        if (!response.ok) {
+            throw new Error(`Lỗi tải file: ${response.statusText}`);
+        }
         const data = await response.json();
         return data;
     } catch (error) {
