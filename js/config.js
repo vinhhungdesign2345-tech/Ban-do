15db139f6476ae44145b9d33328f4c2b5143072a
vinhhungdesign2345-tech <vinhhungdesign2345@gmail.com>
2026-07-31 17:24:12 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,9 @@ const CONFIG = {
         { id: "CaMau", name: "Cà Mau", file: "./geojson/Ca-Mau.geojson", center: [105.15, 9.18] }
     ],
 
+    SHEET_DATA_URL: 'https://script.google.com/macros/s/AKfycbz87dcUkndM5w5BeFqUFYJt8JDEcPu98IH5mbzNdov_6eXTNUEhIiknFQ9P7H2c0ZQE/exec'
+};
+
     // Nền bản đồ Google Vệ tinh
     MAP_STYLE: {
         'version': 8,
